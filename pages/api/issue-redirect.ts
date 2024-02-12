36c3e943e6db3cae2158d73387b49f5df5945fe4
omurovec <owenmurovec@gmail.com>
2024-02-12 10:13:35 -0800

    fix: preview url redirect

--- pages/api/issue-redirect.ts
+++ pages/api/issue-redirect.ts
@@ -1,20 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { fetchGfisData } from "./pse-gfis";
+import { cachePreviewData, cachedPreviewData } from "./cache-previews";
+import { MAX_NUM_PREVIEWS } from "./constants";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  fetchGfisData()
-    .then((data) => {
-      const issueData =
-        data?.[Number(req.query.repoIndex ?? 0)]?.issues[
-          Number(req.query.issueIndex ?? 0)
-        ];
-      res.status(302).redirect(issueData?.url ?? "/");
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: "An error occurred while processing the request.",
-        err,
-      });
+  if (req.query.issueIndex === undefined) {
+    return res.status(400).json({
+      error: "issueIndex is required.",
     });
+  }
+
+  if (Number(req.query.issueIndex) > MAX_NUM_PREVIEWS - 1) {
+    return res.status(400).json({
+      error:
+        "issueIndex is out of bounds. Max value is " + (MAX_NUM_PREVIEWS - 1),
+    });
+  }
+
+  if (cachedPreviewData === undefined) {
+    await cachePreviewData();
+  }
+
+  try {
+    const data = cachedPreviewData[Number(req.query.issueIndex) || 0];
+    res.status(302).redirect(data?.issue.url ?? "/");
+  } catch (err) {
+    res.status(500).json({
+      error: "An error occurred while processing the request.",
+      err,
+    });
+  }
 };
