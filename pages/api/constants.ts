1d93ee659a3de35ccc74f4cd05b1d19efa61c8c1
omurovec <owenmurovec@gmail.com>
2024-06-04 09:04:29 -0700

    Remove Unirep

--- pages/api/constants.ts
+++ pages/api/constants.ts
@@ -2,11 +2,11 @@ import { Entity } from "./types";
 
 export const PSE_ENTITIES: Entity[] = [
   { org: "semaphore-protocol" },
-  { org: "Unirep" },
+  // { org: "Unirep" },
   { org: "Rate-Limiting-Nullifier" },
   { org: "privacy-scaling-explorations" },
   { org: "bandada-infra" },
-  { org: "anon-aadhaar" },
+  { org: "anon-aadhaar" }
   // { repo: { owner: "privacy-scaling-explorations", repo: "maci" } },
   // { repo: { owner: "semaphore-protocol", repo: "semaphore" } },
   // Add more orgs and repos here
